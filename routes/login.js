22c89dfe6f78fe728302875d4df42df47d68d968
siwaniagrawal <siwaniagrawal18@gmail.com>
2018-04-18 20:55:20 +0530

    my project

---
+++ routes/login.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+console.log("rechead");
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('login.ejs', { title: 'login_page' });
+});
+
+module.exports = router;
